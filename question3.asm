f1c924d28181d293c5426d9d862ead0dec32391c
Shikhar Agnihotri <developer@shikharagnihotri.com>
2018-05-25 10:11:10 +0530

    Update question3.asm

--- question3.asm
+++ question3.asm
@@ -1,11 +1,11 @@
 data segment
-    	msg1 db "Soumya Himanish Mohapatra$"
+    msg1 db "Soumya Himanish Mohapatra$"
 data ends
 
 code segment
 assume cs:code,ds:data
 
-print proc 
+print proc
 	lea dx,msg1
 	mov ah,09h
 	int 21h
@@ -13,29 +13,29 @@ print proc
 print endp
 
 mov_cur proc
-	mov ah,02h
-	mov bh,00h
-	mov dh,0ah
-	mov dl,0ch
-	int 10h	
+	mov ah,02h ;    st cursor position and bg colors if any
+	mov bh,00h ;    first digit is
+	mov dh,0ah ;    y axis
+	mov dl,1fh ;    x axis
+	int 10h
 	ret
 mov_cur endp
 
 back_color proc
 	mov ah,06h
-	mov al,00h
-	mov bh,24h
-	mov cx,0
-	mov dx,184fh
+    mov al,00h      ;scroll up window
+	mov bh,24h      ;2 is bg 4 is text color
+	mov cx,0        ;CH = Upper row number, CL = Left column number
+	mov dx,184fh    ;DH = Lower row number, DL = Right column number
 	int 10h
 	ret
 back_color endp
 
 change_color proc
 	mov ah,09h
-	mov bl,12
-	mov bh,0
-	mov cx,25
+	mov bl,12   ;   color
+	mov bh,0    ;   page number
+	mov cx,25   ;   no. of times to print char
 	int 10h
 	ret
 change_color endp
@@ -45,19 +45,19 @@ blink_text proc
 	mov al,00h
 	mov bh,1ch
 	mov cx,0
-	mov dx,184fh	
+	mov dx,184fh
 	int 10h
 	ret
 blink_text endp
 
-start:    
-    	mov ax,data
-	mov ds,ax	
-	
+start:
+    mov ax,data
+	mov ds,ax
+
 blinkLoop:
-	mov si,0004h
+	mov si,0004h    ;brings delay
 	call mov_cur
-	call back_color	
+	call back_color
 	call print
 
 delay1:
@@ -65,20 +65,20 @@ delay1:
 	cmp ax,0
 	dec si
 	jnz delay1
-	
+
 	call mov_cur
 	call blink_text
-	call print	
-	
+	call print
+
 	mov si,0004h
 delay2:
 	mov ax,si
 	cmp ax,0
 	dec si
 	jnz delay2
-	
-	jmp blinkLoop    
-	
+
+	jmp blinkLoop
+
 exit:
 	mov ah,4ch
 	int 21h
